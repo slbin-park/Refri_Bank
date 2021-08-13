650c264b9873e84976b7ffe38206c665faf61a11
jwseo4074 <jwseo4074@pusan.ac.kr>
2021-08-13 13:57:02 +0900

    Change design with jojaeng2

--- src/components/mainpage/aside.js
+++ src/components/mainpage/aside.js
@@ -1,26 +1,29 @@
-import React , { useState} from "react";
+import React, { useState } from "react";
 import '../../style/mainpage/aside/aside.css';
 
-const Aside = ( {livetable, history} ) => {
+const Aside = ({ livetable, history }) => {
   const [recommand_onoff_btn, setrecommand_onoff_btn] = useState(false);
 
   const Func_recommand_onoff_btn = (e) => {
     setrecommand_onoff_btn(!recommand_onoff_btn)
   }
- 
+
   return (
     <>
-    <p className="hint_msg">3초간 눌러보세요!</p>
-    <div className="Big">
-    {/*  <> 이게 제일 큰 박스인데 지금 부모에서 aside-form 으로 픽스되어있음 */}
-      <button className="recommand_onoff_btn"
-        onClick={()=>Func_recommand_onoff_btn()}>
-           <div className="aside-top-box">
-              <h4> 실시간 Top 10 </h4>
-            </div>
+      <p className="hint_msg">3초간 눌러보세요!</p>
+      <div className="Big">
+        {/*  <> 이게 제일 큰 박스인데 지금 부모에서 aside-form 으로 픽스되어있음 */}
+
+        <button className="recommand_onoff_btn"
+          onClick={() => Func_recommand_onoff_btn()}>
+          <div className="aside-top-box">
+            <h4> 실시간 Top 10 </h4>
+          </div>
         </button>
 
-      
+
+
+
         {/* { recommand_onoff_btn ? 
             <>
             <AsideBottomLi/> 
@@ -30,24 +33,24 @@ const Aside = ( {livetable, history} ) => {
         } */}
         {/* <AsideBottomLi/> */}
         <div className="aside-bottom-box">
-         {livetable && livetable.slice(0,10).map(({ FoodId,FoodN, FoodT, FoodC }, index) => {
-          return (
-             <div className="aside-food-block">
-               <div className="aside-food-num"> 
-                 {index+1}
-               </div>
-               <div className="aside-food-name">
-                   {FoodN}
-               </div>
-               
-             </div>
-           )
-          })} 
-       </div>
-      
+          {livetable && livetable.slice(0, 10).map(({ FoodId, FoodN, FoodT, FoodC }, index) => {
+            return (
+              <div className="aside-food-block">
+                <div className="aside-food-num">
+                  {index + 1}
+                </div>
+                <div className="aside-food-name">
+                  {FoodN}
+                </div>
 
-      
-    </div>
+              </div>
+            )
+          })}
+        </div>
+
+
+
+      </div>
     </>
   )
 }
